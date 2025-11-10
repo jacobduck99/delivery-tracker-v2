2bb34d94cc829a808ce4d7d532013ebf8e1228c1
Jacob Duckworth <jacob.formaz@gmail.com>
2025-11-10 11:06:12 +1100

    Writing react component for runs

--- src/features/runpage/RunPage.jsx
+++ src/features/runpage/RunPage.jsx
@@ -1,1 +1,9 @@
 import { useState } from "react";
+
+export default function Run() {
+    const [delivery, setDelivery] = useState({
+        upcoming: "",
+        in_progress: "",
+        completed: "", 
+    })
+}
