cfeabf060480972488e41080fd1bf72dea6541c5
Jacob Duckworth <jacob.formaz@gmail.com>
2025-11-10 12:43:42 +1100

    Fixed typos and adding useeffect

--- src/features/runpage/RunPage.jsx
+++ src/features/runpage/RunPage.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getDrops } from "../../lib/api/runApi.js"; 
 
 export default function Run() {
-    const [delivery, setDelivery] = useState({
-        upcoming: "",
-        in_progress: "",
-        completed: "", 
-    })
+    const [drops, setDrops] = useState([])
 }
