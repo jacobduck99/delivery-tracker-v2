f25bfc7f96626b3713ca37fbb6493092e816f26f
Jacob Duckworth <jacob.formaz@gmail.com>
2025-11-22 14:25:00 +1100

    Changed schema for config

--- server/data/schema.sql
+++ server/data/schema.sql
@@ -3,25 +3,29 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS users (
-  id              integer primary key autoincrement,
-  email           text not null unique,
-  password_hash   text not null,
-  status          integer not null default 1,
-  created_at      text default current_timestamp
+    id              integer primary key autoincrement,
+    email           text not null unique,
+    password_hash   text not null,
+    status          integer not null default 1,
+    created_at      text default current_timestamp
 );
 
 CREATE TABLE IF NOT EXISTS config (
-  van_number           INTEGER NOT NULL,
-  van_name             TEXT    NOT NULL,
-  start_time           TEXT    NOT NULL,
-  first_break          TEXT,
-  second_break         TEXT,
-  end_time             TEXT,
-  number_of_drops      INTEGER NOT NULL CHECK (number_of_drops >= 0),
-  truck_damage         TEXT DEFAULT NULL
+    id              integer primary key autoincrement,
+    user_id         integer not null,
+    van_number           INTEGER NOT NULL,
+    van_name             TEXT    NOT NULL,
+    start_time           TEXT    NOT NULL,
+    first_break          TEXT,
+    second_break         TEXT,
+    end_time             TEXT,
+    number_of_drops      INTEGER NOT NULL CHECK (number_of_drops >= 0),
+    truck_damage         TEXT DEFAULT NULL,
+    foreign key(user_id) references users(id) on delete cascade
 );
 
--- ✅ FIX THESE
+
+CREATE INDEX IF NOT EXISTS idx_config_config_user_id ON config(user_id);
 CREATE INDEX IF NOT EXISTS idx_config_start ON config(start_time);
 CREATE INDEX IF NOT EXISTS idx_config_van   ON config(van_number, start_time);
 
