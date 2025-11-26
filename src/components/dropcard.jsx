d73a170aedb1d2ab93a5c7810b02809f72ecbcbb
Jacob Duckworth <jacob.formaz@gmail.com>
2025-11-26 15:42:55 +1100

    Now all drops have gps and state is in the drop card for address

--- src/components/dropcard.jsx
+++ src/components/dropcard.jsx
@@ -1,27 +1,29 @@
 import { useState, useEffect } from "react";
-import Gps from "../components/gps.jsx";
+import Gps from "./gps.jsx";
+import { startGps } from "./nav.js";
 
 export default function Dropcard({ drop, index }) {
-  return (
+    const [address, setAddress] = useState("");
+
+
+    function onStart() {
+        startGps(address); 
+        };
+    
+    return (
     <div className="card">
       <h2>Drop {index + 1}</h2>
       <pre style={{ background: "#eee", padding: "6px" }}>
-        {JSON.stringify(drop, null, 2)}
+
       </pre>
+    <Gps 
+        address={address}
+        setAddress={setAddress}
+        onStart={onStart}
+        />
     </div>
   );
 }
-    function onStart() {
-       startGps(); 
-    };
-
-    function update(value) {
-        setAddress((prev) => ({ ...prev, value }));
-    }
 
 
-    <Gps 
-        address={drop.address}
-        setAddress={drop.address}
-        onStart={onStart}
-        />
+
