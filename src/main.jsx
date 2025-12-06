aeeafdabe9e4aa94626452d649e16f30e1f9ce25
Jacob Duckworth <jacob.formaz@gmail.com>
2025-12-06 13:28:50 +1100

    Added service worker and then called it in main.jsx so boots up service worker at the very start of the app

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import './index.css'
 import './App.css'
 import App from './App.jsx'
 
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(reg => console.log("SW registered:", reg))
+    .catch(err => console.error("SW failed:", err));
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
