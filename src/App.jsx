54b5a889d63749f4c10760b3b6257ddf5eacf8cf
Jacob Duckworth <jacob.formaz@gmail.com>
2025-12-06 13:53:14 +1100

    Made / redirect to login so now on pwa when you download it to homescreen it asks you to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./features/auth/LoginPage.jsx";
 import SignupPage from "./features/auth/SignupPage.jsx";
 import ConfigPage from "./features/config/ConfigPage.jsx";
@@ -8,10 +8,12 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/config" element={<ConfigPage />} />
         <Route path="/run" element={<RunPage />} />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
