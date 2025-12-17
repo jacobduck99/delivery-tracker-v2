18eb59588f9b7aecd83a76f039fe898678dbe143
Jacob Duckworth <jacob.formaz@gmail.com>
2025-12-17 17:27:06 +1100

    Writing the calculations to send to the api still

--- server/routes/stats_routes.py
+++ server/routes/stats_routes.py
@@ -28,13 +28,32 @@ def get_stats(run_id):
     )
     rows = cur.fetchall()
 
+    cur = conn.execute(
+        """
+        SELECT 
+           start_time,
+           end_time,
+           actual_end_time,
+           truck_damage
+           FROM config
+           WHERE run_id = ?
+        """,
+        (run_id,),
+    )
+    config_row = cur.fetchone()
+
     deliveries = [dict(row) for row in rows]
     
     drops = len(deliveries)
 
     total_elapsed = 0
     for time in deliveries:
+        if time["elapsed"] == 0:
+            continue
         total_elapsed += time["elapsed"]
     avg_drop = total_elapsed / drops
+   
+    shift_duration = config_row["end_time"] - config_row["start_time"] 
+
     
 
