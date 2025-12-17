2ca2199295865793443a088cce4643063c3b08e1
Jacob Duckworth <jacob.formaz@gmail.com>
2025-12-18 10:19:13 +1100

    Change vite config for shad cn

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       "/api": {
@@ -14,4 +22,5 @@ export default defineConfig({
     },
   },
 });
+
 
