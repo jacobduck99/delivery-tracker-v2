6b8d7e90f1b3e94b5b7082a22dd54437c74a16ed
Jacob Duckworth <jacob.formaz@gmail.com>
2025-12-22 10:09:37 +1100

    Changing div in accounts page

--- src/features/accountpage/AccountPage.jsx
+++ src/features/accountpage/AccountPage.jsx
@@ -6,8 +6,7 @@ export default function AccountPage({ logoutUser }) {
     }
 
     return (
-    <div className="flex justify-center mt-19">
-    <button className="w-50 bg-red-600 text-white py-2.5 rounded-full font-semibold hover:bg-red-700" onClick={handleOnClick}>logout</button>
+    <div className="border border-red">
     </div>
     )
 }
