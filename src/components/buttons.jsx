771a596b24be00a9e20c5b17474926007abfe376
Jacob Duckworth <jacob.formaz@gmail.com>
2025-12-22 11:47:02 +1100

    Create button for logout to export to accounts page

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -47,6 +47,12 @@ export function EndShiftModal({ showModal, handleEndShift, setIsEndShiftVisible 
         </button>
         </div>
 
-
     ) 
 } 
+
+export function Logoutbtn({ handleOnClick }) {
+    return (
+    <div className="flex justify-center mt-19">
+    <button className="w-50 bg-red-600 text-white py-2.5 rounded-full font-semibold hover:bg-red-700" onClick={handleOnClick}>logout</button>
+)
+}
--- src/features/accountpage/AccountPage.jsx
+++ src/features/accountpage/AccountPage.jsx
@@ -16,7 +16,6 @@ return (
       
       {/* TOP BAR */}
       <div className="h-[10rem] border border-blue-600 flex items-center justify-center">
-        another
       </div>
 
       {/* MAIN CONTENT ROW */}
@@ -36,9 +35,9 @@ return (
         <div className="account-main flex flex-col flex-1 border border-orange-400">
           <div className="account-header h-[10rem] border border-pink-400" />
 
-          <div className="account-body flex justify-center gap-10 flex-1 border border-red-400">
-            <div>name</div>
-            <div>Pick which mode</div>
+          <div className="account-body flex justify-left gap-10 flex-1 items-left border border-red-400 ml-10">
+            <div className="border border-pink-500 h-[10rem] w-[20rem] p-6">name</div>
+            <div className="border border-pink-500 h-[10rem] w-[20rem] p-6">pick</div>
           </div>
         </div>
 
