161dc6f81394b89e786178253889826bf22f17f1
Jacob Duckworth <jacob.formaz@gmail.com>
2025-12-22 11:36:17 +1100

    Changed layout for html getting ready to add css

--- src/features/accountpage/AccountPage.jsx
+++ src/features/accountpage/AccountPage.jsx
@@ -6,33 +6,48 @@ export default function AccountPage({ logoutUser }) {
     }
 
 
-    return (
-      <div className="account-page flex border border-red-400 p-4 h-screen w-screen">
-        hi
 
-        <div className="account-sidebar flex flex-col justify-between border border-gray-400 h-[40rem] w-[30rem] ml-120 mt-60">
+
+return (
+  <div className="account-page min-h-screen w-screen flex justify-center border border-red-400 mt-20">
+    
+    {/* CONTENT CONTAINER */}
+    <div className="w-full max-w-[100rem] flex flex-col p-4">
+      
+      {/* TOP BAR */}
+      <div className="h-[10rem] border border-blue-600 flex items-center justify-center">
+        another
+      </div>
+
+      {/* MAIN CONTENT ROW */}
+      <div className="flex gap-10 mt-4">
+        
+        {/* SIDEBAR */}
+        <div className="account-sidebar flex flex-col justify-between border border-gray-400 h-[40rem] w-[30rem]">
           <div className="sidebar-section border border-blue-400 h-[20rem]">
             box 1
           </div>
-
           <div className="sidebar-section border border-blue-400 h-[20rem]">
             box 2
           </div>
         </div>
 
-    <div className="account-main flex flex-col h-[50rem] w-[60rem] ml-10 mt-60 border border-orange-400">
-      <div className="account-header h-[10rem] w-full border border-pink-400" />
+        {/* MAIN PANEL */}
+        <div className="account-main flex flex-col flex-1 border border-orange-400">
+          <div className="account-header h-[10rem] border border-pink-400" />
 
-      <div className="account-body flex justify-center w-full space-x-10 border border-red-400">
-        <div className="">name</div>
-
-        <div>Pick which mode</div>
-
-    </div>
-    </div>
+          <div className="account-body flex justify-center gap-10 flex-1 border border-red-400">
+            <div>name</div>
+            <div>Pick which mode</div>
+          </div>
+        </div>
 
       </div>
-    );
+    </div>
+  </div>
+);
+
+
 
 }
 
