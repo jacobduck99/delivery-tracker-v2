8203ccf6675eefc3af37eb10ba9b7af62262c7c1
Jacob Duckworth <jacob.formaz@gmail.com>
2025-12-24 23:07:41 +1100

    Change size of circle progress in run page

--- src/components/progresscircle.jsx
+++ src/components/progresscircle.jsx
@@ -9,8 +9,8 @@ export const Card = ({ children }) => (
 export default function CircleProgress({
   completed,
   total,
-  size = 170,        // 👈 smaller overall size
-  strokeWidth = 12,  // 👈 thinner ring
+  size = 140,        // 👈 smaller overall size
+  strokeWidth = 15  // 👈 thinner ring
 }) {
   const radius = (size - strokeWidth) / 2;
   const circumference = 2 * Math.PI * radius;
