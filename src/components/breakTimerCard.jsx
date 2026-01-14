1358540825c0c99f7585f2f653efd85db63d63ec
Jacob Duckworth <jacob.formaz@gmail.com>
2026-01-14 15:50:49 +1100

    Writing the logic for the timer card

--- src/components/breakTimerCard.jsx
+++ src/components/breakTimerCard.jsx
@@ -1,7 +1,20 @@
 import { Card } from "./progresscircle.jsx";
 import { EndBreakTimerBtn } from "./breaktimer.jsx";
+import { useState } from "react";
 
-export default function TimerCard({ breakSelection, setBreakEndAt, setBreakStartAt }) {
+export default function TimerCard({ breakSelection, breakStartAt, setBreakEndAt, setBreakStartAt }) {
+    const [nowMs, setNowMs] = useState(Date.now());
+
+    function durationMS(breakSelection) {
+        const ms = breakSelection * 60000;
+        return ms;
+    }
+
+    const elapsedMs = nowMs - breakStartAt; 
+    const remainingMs = durationMs(breakSelection) - elapsedMs;
+
+    let intervalId = setInterval(() => setNow)
+
   return (
     <div className="min-h-[90dvh] flex items-center justify-center p-4">
       <Card className="flex items-center justify-center">
